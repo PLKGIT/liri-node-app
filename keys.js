d865d53156048e786caf50444f17ed855c6cf9c8
Lammie <plkelly@yahoo.com>
2020-02-07 15:36:54 -0800

    Update README.md.  Initialized package.json.  Installed NPM packages for Axios, Moment, DotEnv, and Spotify.  Created keys.js, liri.js, log.txt, and random.txt.  Configured keys.js.  Configured env.  Configured gitignore. Configured random.txt.

---
+++ keys.js
@@ -0,0 +1,12 @@
+// Pam Kelly's keys.js file
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+// Indicates that the file has been loaded
+console.log('this is loaded');
+
+// Makes Spotify ID and Secret public to other modules
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
---
+++ liri.js
@@ -0,0 +1,48 @@
+// Pam Kelly's liri.js file
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+// NPM Packages Installed
+// ----------------------------------------------------
+
+// Axios - npm install axios | https://www.npmjs.com/package/axios
+// Moment - npm install moment | https://www.npmjs.com/package/moment and https://momentjs.com/docs/
+// DotEnv - npm install dotenv | https://www.npmjs.com/package/dotenv
+// Spotify - npm install --save node-spotify-api | https://www.npmjs.com/package/node-spotify-api
+
+// Setup
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+// Required NPMs
+// ----------------------------------------------------
+
+require("dotenv").config(); // DotEnv
+var keys = require("./keys"); // Spotify
+var axios = require("axios"); // Axios
+var moment = require("moment"); // Moment
+var fs = ("fs"); // File system
+
+// Spotify
+// var spotify = new spotify(keys.spotify);
+
+// LIRI app should take all of the following commands
+// ----------------------------------------------------
+
+// node liri.js concert-this 'artist/band name', if none, console error message
+
+
+// node liri.js spotify-this-song 'song name', if none use 'The Sign'
+
+
+// node liri.js movie-this 'movie name', if none use 'Mr. Nobody'
+
+
+// node liri.js do-what-it-says (in random.txt)
+
+
+
+// Additional functionality
+// -----------------------------
+
+// Output data to log.txt
